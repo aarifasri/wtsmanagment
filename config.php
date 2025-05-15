c9e95fbb6b27ac212b70def2a6235e2267498977
aarifasri <amirulaaarif@gmail.com>
2025-05-15 10:56:15 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json");
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "wtms";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
